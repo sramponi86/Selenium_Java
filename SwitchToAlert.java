26643e5b8c01ad21fe4972f398cec6a1ccd3a134
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2022-06-06 16:46:38 +0200

    Add files via upload

---
+++ SwitchToAlert.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SwitchToAlert {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/diabomba/Desktop/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/switch-window");
+
+        WebElement alert = driver.findElement(By.id("alert-button"));
+        alert.click();
+
+        Alert popup = driver.switchTo().alert();
+        popup.accept();
+
+        driver.quit();
+    }
+}
