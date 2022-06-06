57ee5107a936a8a187f128e4526da3a62d1e7aab
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2022-06-06 17:08:20 +0200

    Add files via upload

---
+++ DragAndDrop.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDrop {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/diabomba/Desktop/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/dragdrop");
+
+        WebElement image = driver.findElement(By.id("image"));
+        WebElement box = driver.findElement(By.id("box"));
+
+        Actions action = new Actions(driver);
+        action.dragAndDrop(image,box).build().perform();
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
+}
+
