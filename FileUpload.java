67e8dac5957b61d1d55936fff11a3a90ac2dd498
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2022-06-08 10:22:54 +0200

    Add files via upload

---
+++ FileUpload.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+
+public class FileUpload {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/diabomba/Desktop/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/fileupload");
+
+        WebElement fileup = driver.findElement(By.id("file-upload-field"));
+        fileup.sendKeys("file-to-upload.png");
+
+        driver.quit();
+    }
+}
