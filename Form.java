00165d91a80f0091b06db4064fed87230ba91cfc
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2022-06-08 14:14:34 +0200

    Update Form.java

--- Form.java
+++ Form.java
@@ -16,33 +16,14 @@ public class Form {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/form");
-
-        submit_form(driver);
+        FormPage form =  new FormPage();
+        form.submit_form(driver);
         waitforbanner(driver);
         extractText(driver);
 
         assertEquals("The form was successfully submitted!",extractText(driver));
 
         driver.quit();
-    }
-
-    public static void submit_form(WebDriver driver)
-    {
-        driver.findElement(By.id("first-name")).sendKeys("John");
-
-        driver.findElement(By.id("last-name")).sendKeys("Doe");
-
-        driver.findElement(By.id("job-title")).sendKeys("QA Engineer");
-
-        driver.findElement(By.id("radio-button-2")).click();
-
-        driver.findElement(By.id("checkbox-2")).click();
-
-        driver.findElement(By.cssSelector("option[value='1']")).click();
-
-        driver.findElement(By.id("datepicker")).sendKeys("05/28/2019");
-        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
-        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
     }
     public static void waitforbanner(WebDriver driver)
     {
