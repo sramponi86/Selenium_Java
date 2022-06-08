0d157d523ae0f463262a3b6e10361c7f13d7006a
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2022-06-08 10:18:45 +0200

    Add files via upload

---
+++ Dropdown.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Dropdown {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/diabomba/Desktop/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/dropdown");
+
+        WebElement dropdown = driver.findElement(By.id("dropdownMenuButton"));
+        dropdown.click();
+        WebElement option1 = driver.findElement(By.id("autocomplete"));
+        option1.click();
+
+        driver.quit();
+    }
+}
